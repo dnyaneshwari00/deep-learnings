17ac9a66f7505addf600b3d17301bc97fabb2b8e
Rameshwari <badarkherame18@gmail.com>
2025-03-26 22:50:43 +0530

    Day 1 - 9

--- src/split.py
+++ src/split.py
@@ -9,27 +9,26 @@ import random
 from get_data import get_data, read_params
 
 
-
-
 def train_and_test(config_file):
     config = get_data(config_file)
-    root_dir = config['raw_data']['data_src']
-    dest = config['load_data']['preproseesd_data']
-    p = config['load_data']['full_path']
-    cla = config['load_data']['num_classes']
+    root_dir = config['raw_data']['data_src'] #to access main sorce of data...........['raw_data']['data_src'] this 2 terms are copied from params.yaml
+    dest = config['load_data']['preproseesd_data'] # to save data
+    p = config['load_data']['full_path'] #p=full path
+    cla = config['load_data']['num_classes']  
 
     splitr = config['train']['split_ratio']
 
-    for k in range(cla):
+    for k in range(cla): # number of classes i.e 4
         per = len(os.listdir((os.path.join(root_dir, cla[k]))))
         print(k, "->", per)
         cnt = 0
-        split_ratio = round((splitr/100)*per)
+        split_ratio = round((splitr/100)*per) #spliter is having my training data
+        
         for j in os.listdir((os.path.join(root_dir,cla[k]))):
             pat = os.path.join(root_dir+'/'+cla[k])
             print(pat)
             if (cnt!=split_ratio):
-                shutil.copy(pat, dest+'/'+'train'+'/'+'class_'+str(k))
+                shutil.copy(pat, dest+'/'+'train'+'/'+'class_'+str(k)) #k will be having index
                 cnt+=1
             else:
                 shutil.copy(pat, dest+'/'+'test'+'/'+'class_'+str(k))
@@ -45,4 +44,3 @@ if __name__=='__main__':
     args.add_argument('--config',default='params.yaml')
     passed_args=args.parse_args()
     train_and_test(config_file=passed_args.config)
-
